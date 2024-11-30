67defa9930e392940904b8637ffa261e1cc524f1
Max Antipin <max.v.antipin@gmail.com>
2024-11-30 14:03:45 +0300

    Add email validation

--- src/Controller/OneTimePasswordController.php
+++ src/Controller/OneTimePasswordController.php
@@ -4,18 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/register', name:'register-')]
 class OneTimePasswordController extends AbstractController
 {
-    #[Route('/send-otp', name:'send-otp', methods:'POST')]
-    public function send(): JsonResponse
+    #[Route('/send-otp', name:'send-otp', methods:'POST', format: 'json')]
+    public function send(Request $request, ValidatorInterface $validator): JsonResponse
     {
+        $email = $request->request->getString('uid');
+        $emailConstraint = new Assert\Email();
+        // all constraint "options" can be set this way
+        // $emailConstraint->message = 'Invalid email address';
+        $errors = $validator->validate(
+            $email,
+            [new Assert\NotBlank(), $emailConstraint]
+        );
+        if ($errors->count()) {
+            $errorMessage = $errors[0]->getMessage();
+            return $this->json(['message' => $errorMessage], 422);
+        }
         return $this->json(['send code']);
     }
 
-    #[Route('/verify-otp', name:'verify-otp', methods:'POST')]
+    #[Route('/verify-otp', name:'verify-otp', methods:'POST', format: 'json')]
     public function verify(): JsonResponse
     {
         return $this->json(['verify code']);
--- tests/Controller/OneTimePasswordControllerTest.php
+++ tests/Controller/OneTimePasswordControllerTest.php
@@ -7,6 +7,7 @@ namespace App\Tests\Controller;
 use App\Controller\OneTimePasswordController;
 use App\Tests\AppWebTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[CoversClass(OneTimePasswordController::class)]
 class OneTimePasswordControllerTest extends AppWebTestCase
@@ -14,9 +15,30 @@ class OneTimePasswordControllerTest extends AppWebTestCase
     public function testSend(): void
     {
         $client = static::createClient();
-        $client->request('POST', $this->generateUrl('register-send-otp'));
+        $client->request(
+            'POST',
+            $this->generateUrl('register-send-otp'),
+            ['uid' => 'max.v.antipin@gmail.com']
+        );
         $this->assertResponseIsSuccessful();
         $this->assertResponseFormatSame('json');
+        /** @var JsonResponse $response */
+        $response = $client->getResponse();
+        var_dump($response->getContent());
+    }
+
+    public function testSendEmptyEmail(): void
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            $this->generateUrl('register-send-otp'),
+        );
+        $this->assertResponseStatusCodeSame(422);
+        $this->assertResponseFormatSame('json');
+        /** @var JsonResponse $response */
+        $response = $client->getResponse();
+        var_dump(json_decode($response->getContent()));
     }
 
     public function testVerify(): void
@@ -25,5 +47,8 @@ class OneTimePasswordControllerTest extends AppWebTestCase
         $client->request('POST', $this->generateUrl('register-verify-otp'));
         $this->assertResponseIsSuccessful();
         $this->assertResponseFormatSame('json');
+        /** @var JsonResponse $response */
+        $response = $client->getResponse();
+        // var_dump($response->getContent());
     }
 }
